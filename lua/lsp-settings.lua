7f822c671f3e743ffe2c38289d6fb069ba602ee7
dl1987 <dawid.lapczynski@gmail.com>
2024-01-11 15:11:30 +0100

    lsp settings moved to lazy

--- lua/lsp-settings.lua
+++ lua/lsp-settings.lua
@@ -1,17 +1,21 @@
 local M = {}
 
-vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = false,
-})
+function M:configure()
 
-local signs = { Error = "●", Warn = "●", Hint = "●", Info = "●" }
-for type, icon in pairs(signs) do
+  vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = false,
+  })
+
+  local signs = { Error = "●", Warn = "●", Hint = "●", Info = "●" }
+  for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
+
 end
 
 local opts = { noremap = true, silent = true}
@@ -31,30 +35,28 @@ local on_attach_clangd = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd> lua vim.lsp.buf.code_action({apply=true})<CR>', opts)
 end
 
-function M:configure()
-  require('lspconfig')['clangd'].setup {
-    on_attach = on_attach_clangd,
-    cmd = {'clangd','--query-driver=/home/dawlap/.sdk/sysroots/x86_64-acu6sdk-linux/usr/bin/aarch64-acu6-linux/aarch64-acu6-linux-g++', '--clang-tidy'},
-  }
+require('lspconfig')['clangd'].setup {
+  on_attach = on_attach_clangd,
+  cmd = {'clangd','--query-driver=/home/dawlap/.sdk/sysroots/x86_64-acu6sdk-linux/usr/bin/aarch64-acu6-linux/aarch64-acu6-linux-g++', '--clang-tidy'},
+}
 
-  require('lspconfig')['pyright'].setup {
-    on_attach = on_attach
-  }
-  require('lspconfig').pylsp.setup{
-    settings ={
-      pyls = {
-        plugins = {
-          pylint = {enabled = true},
-          flake8 = {enabled = true},
+require('lspconfig')['pyright'].setup {
+  on_attach = on_attach
+}
+require('lspconfig').pylsp.setup{
+  settings ={
+    pyls = {
+      plugins = {
+        pylint = {enabled = true},
+        flake8 = {enabled = true},
 
-        }
       }
     }
   }
+}
 
-  require('lspconfig').cmake.setup {
-    buildDirectory = "build"
-  }
-end
+require('lspconfig').cmake.setup {
+  buildDirectory = "build"
+}
 
 return M
